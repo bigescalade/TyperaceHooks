626cacc1e897651925c30886b1d52dfe2621641d
Rob Cunning <rjacunning@gmail.com>
2019-04-06 09:16:23 +1100

    Create react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Typerace from './views/Typerace';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Typerace />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/views/Typerace/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Typerace extends Component {
+  render() {
+    return <p>Typerace</p>;
+  }
+}
+
+export default Typerace;
