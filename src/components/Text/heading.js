1be6c48c5cecd7fcaf3ccdcd1ce4ea82dc52015e
Rob Cunning <rjacunning@gmail.com>
2019-04-06 12:37:31 +1100

    Added Text components

---
+++ src/components/Text/heading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const Heading = props => {
+  return <h1 className="Heading">{props.text}</h1>;
+};
+
+export default Heading;
---
+++ src/components/Text/subheading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const Subheading = props => {
+  return <h2 className="Subheading">{props.text}</h2>;
+};
+
+export default Subheading;
