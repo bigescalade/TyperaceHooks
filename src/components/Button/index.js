fbab647c8c998364a7a7eeeb54a87fbf06f0b940
Rob Cunning <rjacunning@gmail.com>
2019-04-06 13:14:41 +1100

    Added button styles

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './styles.css';
 
 const Button = props => {
-  return <button onClick={props.onClick}>{props.text}</button>;
+  return (
+    <button className="Button" onClick={props.onClick}>
+      {props.text}
+    </button>
+  );
 };
 
 export default Button;
